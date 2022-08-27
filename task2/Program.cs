363f3aa80d2da661e65a22a2b1b01f115b3f119c
Egor <pro100egar@gmail.com>
2022-08-27 12:01:06 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+class Program
+
+{
+    static void FillArray(int[] collection)
+    {
+        int length = collection.Length;
+        int index = 0;
+     while (index < length)
+        {
+            collection[index] = new Random().Next();
+            index++;
+        }
+    }
+    static void PrintArray(int[] col)     
+    {
+        int count = col.Length;
+        int position = 0;
+        while(position < count)
+        {
+            Console.Write(col[position] + ", ");
+            position++;
+        }
+    } 
+    static void Main(string[] args)
+    {
+        int[] array = new int[10];
+        FillArray(array);
+        PrintArray(array);
+        int index = 0;
+        int length = array.Length;
+        int sum = 0;
+
+        while (index < length)
+        {
+            if (index%2 > 0)
+            {
+                sum = array[index] + sum;
+            }
+            index++;
+        }
+        Console.WriteLine(" ");
+        Console.WriteLine("Сумма чисел стоящих на нечётных позициях равна: " + sum + ".");
+    }
+}
